c17dadd2873dd2a46f09e0b99d6c43213b806c59
Lylisse <ulyssegv@gmail.com>
2023-12-22 01:06:28 +0100

    Detect when pot is changed

--- PongPot/PongPot.ino
+++ PongPot/PongPot.ino
@@ -1,5 +1,5 @@
-int pot1Position;
-int pot2Position;
+int PrevPotPos1;
+int PrevPotPos2;
 
 struct Tuple{
   int a;
@@ -13,10 +13,20 @@ void printPotPos(Tuple PotPos){
   Serial.print("\n");
 }
 
+bool isPotChange(Tuple PotPos){
+  if( abs(PrevPotPos1 - PotPos.a) > 5 || abs(PrevPotPos2 - PotPos.b) > 5){
+    PrevPotPos1 = PotPos.a;
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
 Tuple getPotPos(){
-  pot1Position = analogRead(A0);
-  pot2Position = analogRead(A1);
-  return {pot1Position, pot2Position};
+  int Pot1Position = analogRead(A0);
+  int Pot2Position = analogRead(A1);
+  return {Pot1Position, Pot2Position};
 }
 
 void setup() {
@@ -27,6 +37,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 Tuple PotPos = getPotPos();
+if(isPotChange(PotPos)){Serial.print("CHANGEMENT"); Serial.print("\n");}
 int Pot1Pos = PotPos.a;
 int Pot2Pos = PotPos.b;
 /*printPotPos(PotPos);*/
